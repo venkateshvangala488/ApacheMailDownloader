4b165fbe324359a9af576311d335574f8cf5be74
venkatesh-vangala <=>
2014-11-24 17:47:31 +0530

    Refactered the WebCrawler code

--- src/com/imaginea/venkatesh/main/WebCrawlerApp.java
+++ src/com/imaginea/venkatesh/main/WebCrawlerApp.java
@@ -11,12 +11,13 @@ public class WebCrawlerApp {
 	public static void main(String args[]) {
 		final Logger LOGGER = Logger.getLogger(WebCrawlerApp.class);
 		LOGGER.info("Main App Intialised....");
+		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
 		LOGGER.info("Please Enter Year.");
 		String year = scanner.next();
 		LOGGER.info("Please Enter Folder Name.");
 		String folderName = scanner.next();
 		ApacheMailDownloader downloader = new ApacheMailDownloader();
-		downloader.StartDownload(year, folderName);
+		downloader.download(year, folderName);
 	}
 }
